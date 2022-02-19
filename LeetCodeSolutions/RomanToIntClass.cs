27d8c1bcea6a066f71632a7cfbff75cb6d7794e6
Mehmet Kemal Bayer <kemal.bayer@gmail.com>
2022-02-19 10:50:49 +0300

    first commmit

---
+++ LeetCodeSolutions/RomanToIntClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    public class RomanToIntClass
+    {
+        public int RomanToInt(string s)
+        {
+            int total = 0;
+            char next;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                int addOrSubstract = 1;
+                if (i + 1 == s.Length)
+                {
+                    next = ' ';
+                }
+                else
+                {
+                    next = s[i + 1];
+                }                
+                if (c == 'I' && next == 'V')
+                {
+                    addOrSubstract = -1;
+                }
+                if (c == 'I' && next == 'X')
+                {
+                    addOrSubstract = -1;
+                }
+                if (c == 'X' && next == 'L')
+                {
+                    addOrSubstract = -1;
+                }
+                if (c == 'X' && next == 'C')
+                {
+                    addOrSubstract = -1;
+                }
+                if (c == 'C' && next == 'D')
+                {
+                    addOrSubstract = -1;
+                }
+                if (c == 'C' && next == 'M')
+                {
+                    addOrSubstract = -1;
+                }
+                total += GetValue(c, addOrSubstract);
+            }
+            return total;
+        }
+        public int GetValue(char c, int addOrSubstract)
+        {
+            if (c == 'I') return 1 * addOrSubstract;
+            if (c == 'V') return 5 * addOrSubstract;
+            if (c == 'X') return 10 * addOrSubstract;
+            if (c == 'L') return 50 * addOrSubstract;
+            if (c == 'C') return 100 * addOrSubstract;
+            if (c == 'D') return 500 * addOrSubstract;
+            if (c == 'M') return 1000 * addOrSubstract;
+            throw new Exception();
+        }
+    }
+}
---
+++ LeetCodeTests/RomanToIntTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using LeetCodeSolutions;
+namespace LeetCodeTests
+{
+    public class RomanToIntTests
+    {
+        RomanToIntClass romanToIntClass;        
+        [Fact]
+        public void Returns3ForIII()
+        {
+            //Arrange
+            romanToIntClass = new RomanToIntClass();
+            //Act
+            int result = romanToIntClass.RomanToInt("III");
+            //Assert
+            Assert.True(result == 3);
+        }
+        [Fact]
+        public void Returns58ForLVIII()
+        {
+            //Arrange
+            romanToIntClass = new RomanToIntClass();
+            //Act
+            int result = romanToIntClass.RomanToInt("LVIII");
+            //Assert
+            Assert.Equal(58, result);
+        }
+        [Fact]
+        public void Returns1994ForMCMXCIV()
+        {
+            //Arrange
+            romanToIntClass = new RomanToIntClass();
+            //Act
+            int result = romanToIntClass.RomanToInt("MCMXCIV");
+            //Assert
+            Assert.Equal(1994, result);
+        }
+        [Fact]
+        public void Returns9ForIX()
+        {
+            //Arrange
+            romanToIntClass = new RomanToIntClass();
+            //Act
+            int result = romanToIntClass.RomanToInt("IX");
+            //Assert
+            Assert.Equal(9, result);
+        }
+    }
+}
