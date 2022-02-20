b029666ea0ba9701dd5db18143cae6e94241a3c3
mkb <kemal.bayer@gmail.com>
2022-02-20 19:48:03 +0300

    adds 28.strStr problem

---
+++ LeetCodeSolutions/StrStrProblem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LeetCodeSolutions
+{
+    public class StrStrProblem
+    {
+        public StrStrProblem()
+        {
+        }
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Equals(String.Empty)) return 0;
+            return haystack.IndexOf(needle);
+        }
+    }
+}
