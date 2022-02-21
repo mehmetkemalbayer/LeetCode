b31bc179f14159c3892d8dd955bcf2ab067ddf2c
mkb <kemal.bayer@gmail.com>
2022-02-21 10:35:38 +0300

    adds maxsubarray problem

---
+++ LeetCodeSolutions/MaxSubArrayProblem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCodeSolutions
+{
+    public class MaxSubArrayProblem
+    {
+        public MaxSubArrayProblem()
+        {
+            
+        }
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = int.MinValue;
+            int curSum = 0;
+            for(int i=0;i<nums.Length;i++)
+            {
+                curSum += nums[i];
+                if (curSum> maxSum)
+                {
+                    maxSum = curSum;
+                }
+                if (curSum <0)
+                {
+                    curSum = 0;
+                }
+            }
+            return maxSum;
+        }
+        // [-2,-1,-4,0,-1,1,2,-3,1]
+        // [-2,-3]
+        // [-2,-1]
+        // [5,9,8,15,23]
+        // [-1,-3]
+        //5, 4, -1, 7, 8
+    }
+}
---
+++ LeetCodeTests/MaxSubArrayTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xunit;
+using LeetCodeSolutions;
+namespace LeetCodeTests
+{
+    public class MaxSubArrayTests
+    {
+        MaxSubArrayProblem problem;
+        public MaxSubArrayTests()
+        {
+            problem = new MaxSubArrayProblem();
+        }
+        [Fact]
+        public void Returns6 ()
+        {
+            int result =problem.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+            Assert.Equal(6, result);
+        }
+        [Fact]
+        public void Returns1()
+        {
+            int result = problem.MaxSubArray(new int[] {1});
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void Returns23()
+        {
+            int result = problem.MaxSubArray(new int[] { 5, 4, -1, 7, 8 });
+            Assert.Equal(23, result);
+        }
+        [Fact]
+        public void Returns1_2()
+        {
+            int result = problem.MaxSubArray(new int[] { -2, 1 });
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void ReturnsMinus1()
+        {
+            int result = problem.MaxSubArray(new int[] { -1 });
+            Assert.Equal(-1, result);
+        }
+        [Fact]
+        public void ReturnsMinus1_2()
+        {
+            int result = problem.MaxSubArray(new int[] { -2, -1 });
+            Assert.Equal(-1, result);
+        }
+        [Fact]
+        public void ReturnsMinus1_3()
+        {
+            int result = problem.MaxSubArray(new int[] { -1, -2 });
+            Assert.Equal(-1, result);
+        }
+    }
+}
